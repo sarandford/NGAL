769f0a1b22a2e55328f8b8599798a05bd9a2f59b
sarandford <fordsr@g.cofc.edu>
2015-06-06 16:51:41 -0400

    another try with heroku

--- db/migrate/20150603211840_add_user_to_gl.rb
+++
@@ -1,5 +0,0 @@
-class AddUserToGl < ActiveRecord::Migration
-  def change
-	add_column :grocery_lists, :user_id, :int
-  end
-end
--- db/migrate/20150603212514_add_food_to_gl.rb
+++
@@ -1,5 +0,0 @@
-class AddFoodToGl < ActiveRecord::Migration
-  def change
-  	add_column :grocery_lists, :food_id, :int
-  end
-end
