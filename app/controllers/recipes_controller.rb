42d395c633215ea50872990c64a162c17c89912b
sarandford <fordsr@g.cofc.edu>
2015-06-16 14:51:59 -0400

    johns second

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,8 +15,8 @@ class RecipesController < ApplicationController
 		response = RestClient.get "http://food2fork.com/api/search?key=c3c2406305ef3e33c68210f57d5f0719&q=#{@ingredients}"
 		response = JSON.parse response
 		puts response.inspect
-		for item in response[:recipes] do
-			@urls << item[:source_url]
+		for item in response["recipes"] do
+			@urls << item["source_url"]
 		end
 	end
 
