62a7260e2ec6342a1ece85d2a3a9379e40b6659e
sarandford <fordsr@g.cofc.edu>
2015-07-20 20:58:32 -0400

    fix heroku api

--- app/api/v1/data.rb
+++ app/api/v1/data.rb
@@ -13,7 +13,9 @@ module V1
         end
 
         get :search do
-          RestClient.get "http://food2fork.com/api/search?key=c3c2406305ef3e33c68210f57d5f0719&q=#{params[:ingredients]}"
+          safe_url = URI.encode("http://food2fork.com/api/search?key=c3c2406305ef3e33c68210f57d5f0719&q=#{params[:ingredients]}".strip)
+
+          RestClient.get safe_url
         end
 
      
